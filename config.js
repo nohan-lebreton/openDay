38af7495bf389c24ef432ca619da5522f960879f
lebre <lebreton.nohan@gmail.com>
2024-11-29 12:30:36 +0100

    fix: renovate

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
     extends: ["config:base"],
+    platform: "github",
+    autodiscorver: true,
     repositories : ["nohan-lebreton/openDay"]  // Hérite des configurations par défaut de Renovate
   };
   
